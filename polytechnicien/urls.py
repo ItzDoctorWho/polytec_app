65639680155fcbef99f08635602aa68cc044f4c3
Essouaied Med Helmi <essouaiedmedhelmi@gmail.com>
2025-02-21 03:49:48 +0100

    Add member management routes

--- polytechnicien/urls.py
+++ polytechnicien/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('members/', views.members_list, name='members_list'),
+    path("members/update/<int:member_id>/", views.update_member, name="update_member"),
+    path("members/delete/<int:member_id>/", views.delete_member, name="delete_member"),
     path('polytechnicien/<int:member_id>/', views.member_view, name='polytechnicien')
 ]
